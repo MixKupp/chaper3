4dcc2d48a3c8594d14ac008e06387f553c1f76a9
Mixkupp <mixkup.thiwongsa@gmail.com>
2024-07-23 23:21:03 +0700

    1

---
+++ src/main/java/se233/chapter3/model/FileFreq.java
@@ -0,0 +1,21 @@
+package se233.chapter3.model;
+
+public class FileFreq {
+
+    public String getPath() {
+        return path;
+    }
+
+    private String name;
+    private String path;
+    private Integer freq;
+    public FileFreq(String name, String path, Integer freq) {
+        this.name = name;
+        this.path = path;
+        this.freq = freq;
+    }
+    @Override
+    public String toString() {
+        return String.format("{%s:%d}", name, freq);
+    }
+}
---
+++ src/main/java/se233/chapter3/model/PdfDocument.java
@@ -0,0 +1,45 @@
+package se233.chapter3.model;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class PdfDocument {
+    private String name;
+    private String filePath;
+    private PDDocument document;
+
+    public LinkedHashMap<String, List<FileFreq>> getUniqueSets() {
+        return uniqueSets;
+    }
+
+    public void setUniqueSets(LinkedHashMap<String, List<FileFreq>> uniqueSets) {
+        this.uniqueSets = uniqueSets;
+    }
+
+    private LinkedHashMap<String, List<FileFreq>> uniqueSets;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public PDDocument getDocument() {
+        return document;
+    }
+
+    public PdfDocument(String filePath) throws IOException {
+        this.name = Paths.get(filePath).getFileName().toString();
+        this.filePath = filePath;
+        File pdfFile = new File(filePath);
+        this.document = PDDocument.load(pdfFile);
+    }
+
+}
